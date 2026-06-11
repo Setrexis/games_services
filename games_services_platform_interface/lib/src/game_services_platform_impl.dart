8be0333ea892b242ba0d9c8a927a785de05f562c
Sebastian Nadler <sebastian.nadler@tum.de>
2026-06-11 22:53:00 +0200

    Android & darwin time span and player scope support for showLeaderboard

--- games_services_platform_interface/lib/src/game_services_platform_impl.dart
+++ games_services_platform_interface/lib/src/game_services_platform_impl.dart
@@ -28,11 +28,11 @@ class MethodChannelGamesServices extends GamesServicesPlatform {
             .map((json) =>
                 json == null ? null : PlayerData.fromJson(jsonDecode(json)))
             .listen((player) {
-          _player = player;
-          _streamController.add(_player);
+                _player = player;
+                _streamController.add(_player);
         }, onError: (error) {
-          _player = null;
-          _streamController.add(null);
+                _player = null;
+                _streamController.add(_player);
         });
       },
       onCancel: () {
@@ -99,11 +99,14 @@ class MethodChannelGamesServices extends GamesServicesPlatform {
     String iOSLeaderboardID = "",
     String androidLeaderboardID = "",
     TimeScope timeScope = TimeScope.allTime,
+    PlayerScope playerScope = PlayerScope.global,
   }) async {
     return await _methodChannel.invokeMethod("showLeaderboards", {
-      "leaderboardID":
-          Device.isPlatformAndroid ? androidLeaderboardID : iOSLeaderboardID,
-      if (Device.isPlatformAndroid) "span": timeScope.value,
+      "leaderboardID": Device.isPlatformAndroid
+          ? androidLeaderboardID
+          : iOSLeaderboardID,
+      "span": timeScope.value,
+      "leaderboardCollection": playerScope.value,
     });
   }
 
@@ -183,11 +186,11 @@ class MethodChannelGamesServices extends GamesServicesPlatform {
   Future<String?> getAuthCode(String clientID,
           {bool forceRefreshToken = false}) =>
       Device.isPlatformAndroid
-          ? _methodChannel.invokeMethod("getAuthCode", {
-              "clientID": clientID,
-              "forceRefreshToken": forceRefreshToken,
-            })
-          : Future.value(null);
+      ? _methodChannel.invokeMethod("getAuthCode", {
+          "clientID": clientID,
+          "forceRefreshToken": forceRefreshToken,
+        })
+      : Future.value(null);
 
   @override
   Future<String?> showAccessPoint(AccessPointLocation location) async {
@@ -229,7 +232,7 @@ class MethodChannelGamesServices extends GamesServicesPlatform {
 
   @override
   Future<IdentityVerificationSignature?>
-      fetchIdentityVerificationSignature() async {
+  fetchIdentityVerificationSignature() async {
     if (!Device.isPlatformIOS && !Device.isPlatformMacOS) {
       return null;
     }
@@ -240,5 +243,23 @@ class MethodChannelGamesServices extends GamesServicesPlatform {
     }
     return IdentityVerificationSignature.fromJson(
         result.cast<String, dynamic>());
+  }
+}
+
+class _PlayerStreamView extends StreamView<PlayerData?> {
+  _PlayerStreamView(Stream<PlayerData?> stream, this.emit) : super(stream);
+
+  VoidCallback emit;
+
+  @override
+  StreamSubscription<PlayerData?> listen(
+      void Function(PlayerData? value)? onData,
+      {Function? onError,
+    void Function()? onDone,
+      bool? cancelOnError}) {
+    final sub = super.listen(onData,
+        onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+    emit();
+    return sub;
   }
 }
